462c140a80627cd1b7f0c941c0d34872f00007d8
Evandro <evandrorm89@gmail.com>
2023-04-27 15:13:40 -0300

    colocando janela de mensagem falando sobre dificuldades tecnicas com os videos

--- rani_passos/src/app/pages/painel/Painel.tsx
+++ rani_passos/src/app/pages/painel/Painel.tsx
@@ -3,6 +3,14 @@ import * as React from 'react';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { Footer, TopMenu } from '../../shared/components';
 import { AuthProvider, useAuthContext } from '../../shared/contexts';
+import { 
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button
+} from '@mui/material';
 
 import Cursos from '../cursos/Cursos';
 const pages = [
@@ -13,14 +21,21 @@ const pages = [
 
 export const Painel = () => {
   const { isAuth } = useAuthContext();
+  const [openDialog, setOpenDialog] = React.useState(true);
 
   const [actualPage, setActualPage] = React.useState('');
   const location = useLocation();
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
   function getActualPage() {
     const pathname = pages.filter((e) =>  e.url === location.pathname);
     const actualPage = pages.filter(page => page.url === pathname[0].url);
     setActualPage(actualPage[0].name.toUpperCase());
   }
+
   React.useEffect(() => {
     getActualPage();
   }, [actualPage]);
@@ -48,6 +63,23 @@ export const Painel = () => {
       </Box>
       <Cursos />
       <Footer />
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle id="alert-dialog-title">Importante!</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+          Fala, meu lindo e minha linda!
+          Infelizmente tivemos um problema técnico com os nossos vídeos, mas tudo está sendo reparado o mais rápido possível.
+          Em breve o acesso as aulas estará 100% corrigido.
+          Qualquer dúvida é só me chamar no WhastApp: (45) 9 88210669 
+          Agradeço pela compreensão!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} autoFocus>
+            Fechar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
