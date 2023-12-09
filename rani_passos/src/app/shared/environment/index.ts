503cdc936f35434979d08b5345cf2001a358f343
Thiago Malaquias <tammalaquias@gmail.com>
2023-12-09 15:35:18 -0300

    corrigindo area de cursos

--- rani_passos/src/app/shared/environment/index.ts
+++ rani_passos/src/app/shared/environment/index.ts
@@ -2,6 +2,6 @@ export const Environment = {
   LIMITE_DE_LINHAS: 10,
   INPUT_DE_BUSCA: 'Buscar',
   LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
-  // URL_BASE: 'https://www.ranipassos.com.br/',
-  URL_BASE: 'http://localhost:3001/',
+  URL_BASE: 'https://www.ranipassos.com.br/',
+  // URL_BASE: 'http://localhost:3001/',
 };
